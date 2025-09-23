bbf2e84fdf29aa239ffa0df3fc548b3268096d5f
vitosant <vitor.200483@gmail.com>
2025-09-23 08:21:37 -0300

    testing readline function and history

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 13:32:47 by dasimoes          #+#    #+#             */
-/*   Updated: 2025/09/21 13:33:23 by dasimoes         ###   ########.fr       */
+/*   Updated: 2025/09/23 08:10:58 by vitosant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,23 @@
 
 int	main(int ac, char **av, char **env)
 {
-	
-	
+	char	*str;
+
+	(void) ac;
+	(void) av;
+	(void) env;
+	while (1)
+	{
+		str = readline("minishell$ ");
+		if (str == NULL)
+		{
+			printf("exit");
+			break ;
+		}
+		if (*str != '\0')
+			add_history(str);
+		printf("voce digitou: %s\n", str);
+	}
+	rl_clear_history();
 	return (0);
 }
-	
