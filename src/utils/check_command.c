9c88886a3477a3eafa681d84c640ffc8ddd47f37
vitosant <vitor.200483@gmail.com>
2025-10-07 12:25:48 -0300

    create check_command function

---
+++ src/utils/check_command.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_command.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 12:01:22 by vitosant          #+#    #+#             */
+/*   Updated: 2025/10/07 12:04:49 by vitosant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	check_command(t_cmd *node)
+{
+	char	*file;
+
+	file = node->argv[0];
+	if (access(file, F_OK) == -1 || access(file, X_OK) == -1)
+	{
+		perror(file);
+		return (0);
+	}
+	return (1);
+}
