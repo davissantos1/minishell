3961aea8387f2be1fbf7159d40c1b016c03d3d84
vitosant <vitor.200483@gmail.com>
2025-10-07 12:19:17 -0300

    create linked list lstpid to save fork return

--- includes/executor.h
+++ includes/executor.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vitosant <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 15:52:51 by vitosant          #+#    #+#             */
-/*   Updated: 2025/10/06 15:52:53 by vitosant         ###   ########.fr       */
+/*   Updated: 2025/10/07 12:05:07 by vitosant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,18 @@
 # define EXECUTOR_H
 
 # define NODE_SUBSHELL 919
+# define STDIN_FD 1
+# define STDOUT_FD 0
+
+typedef struct	s_lstpid
+{
+	pid_t			pid;
+	struct s_lstpid	*next;
+}					t_lstpid;
+
+void	executor(t_minishell *shell, t_ast *node);
+void	close_pipes(int	*pipe_fd);
+void 	pid_add(t_minishell *shell, pid_t pid);
+int		get_return(t_minishell *shell);
+int		check_command(t_cmd *node);
 #endif
