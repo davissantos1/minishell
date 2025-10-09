d0b9dc275fdfedc061e56a855121734ee7db3355
vitosant <vitor.200483@gmail.com>
2025-10-08 21:04:32 -0300

    replace find_redir to find_heredoc

---
+++ src/execution/find_heredoc.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_redir.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vitosant <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/04 14:18:46 by vitosant          #+#    #+#             */
+/*   Updated: 2025/10/04 14:24:24 by vitosant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	find_heredoc(t_minishell *shell, t_ast *node)
+{
+	t_cmd	*cmd;
+	t_redir	*redir;
+	t_list	*lst;
+
+	if (node->type == NODE_CMD)
+	{
+		cmd = node->data;
+		lst = cmd->redir;
+		while (lst)
+		{
+			redir = lst->content;
+			if (redir->type == NODE_HEREDOC)
+				cmd->std_in = heredoc(redir->file);
+			lst = lst->next;
+		}
+	}
+	if (node->left && shell->exit != -130)
+		find_heredoc(shell, node->left);
+	if (node->right && shell->exit != -130)
+		find_heredoc(shell, node->right);
+}
