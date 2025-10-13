24bc1ab557cc5de09e5b7daf4d9bb36ac5ca70e4
vitosant <vitor.200483@gmail.com>
2025-10-13 15:54:52 -0300

    check if fd is in lstfd to close

--- src/utils/close_redir.c
+++ src/utils/close_redir.c
@@ -3,19 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   close_redir.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vitosant <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 19:40:00 by vitosant          #+#    #+#             */
-/*   Updated: 2025/10/08 19:40:01 by vitosant         ###   ########.fr       */
+/*   Updated: 2025/10/11 13:44:47 by vitosant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static t_lstint	*find_fd(t_cmd *cmd, int fd);
+
 void	close_redir(t_cmd *cmd)
 {
-	if (cmd->std_in != -1 && cmd->std_in != 0)
+	if (cmd->std_in != -1 && cmd->std_in != 0 && !find_fd(cmd, cmd->std_in))
 		close(cmd->std_in);
-	if (cmd->std_out != -1 && cmd->std_in != 1)
+	if (cmd->std_out != -1 && cmd->std_out != 1 && !find_fd(cmd, cmd->std_out))
 		close(cmd->std_out);
+}
+
+static t_lstint	*find_fd(t_cmd *cmd, int fd)
+{
+	t_lstint	*lst;
+
+	lst = cmd->lst_fds;
+	if (!lst)
+		return (NULL);
+	while (lst && lst->value != fd)
+		lst = lst->next;
+	return (lst);
 }
