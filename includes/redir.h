833104584104e7064d12b906db3d81977f106e64
vitosant <vitor.200483@gmail.com>
2025-10-13 15:37:48 -0300

    add heredoc function

--- includes/redir.h
+++ includes/redir.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redir.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vitosant <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 15:53:02 by vitosant          #+#    #+#             */
-/*   Updated: 2025/10/06 15:53:03 by vitosant         ###   ########.fr       */
+/*   Updated: 2025/10/10 13:39:04 by vitosant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # define PATH_MAX 4096
 
+# include "minishell.h"
+
 typedef struct s_redir
 {
 	t_node_type		type;
@@ -22,6 +24,7 @@ typedef struct s_redir
 }	t_redir;
 
 void	find_heredoc(t_minishell *shell, t_ast *node);
+int		heredoc(char *limiter);
 void	redirection(t_minishell *shell, t_cmd *cmd);
 void	close_redir(t_cmd *cmd);
 
