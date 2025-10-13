ac9870ec79cc61d7b19160cd3b990d78742fbe4f
vitosant <vitor.200483@gmail.com>
2025-10-13 15:47:37 -0300

    create bultin function

---
+++ src/execution/builtin.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/10 11:47:50 by vitosant          #+#    #+#             */
+/*   Updated: 2025/10/10 13:44:37 by vitosant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	builtin(t_minishell *shell, t_cmd *cmd)
+{
+	printf("aqui em builtin, %i %i", shell->std_in, cmd->std_in);
+}
