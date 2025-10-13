054fa382611ea69b0ce78ae08b8a7a0955142759
vitosant <vitor.200483@gmail.com>
2025-10-13 15:44:29 -0300

    save last return

--- src/execution/and_handle.c
+++ src/execution/and_handle.c
@@ -6,7 +6,7 @@
 /*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 10:06:15 by vitosant          #+#    #+#             */
-/*   Updated: 2025/09/30 10:10:09 by vitosant         ###   ########.fr       */
+/*   Updated: 2025/10/11 12:13:00 by vitosant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ void	and_node(t_minishell *shell, t_ast *node)
 	grepper = get_return(shell);
 	if (grepper == 0)
 		executor(shell, node->right);
+	shell->exit = grepper;
 }
