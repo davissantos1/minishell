be761ea9b1023f5b8c79a08feec3f8a82f3d9483
vitosant <vitor.200483@gmail.com>
2025-10-13 15:53:54 -0300

    modify to accept builtins

--- src/utils/lst_pid.c
+++ src/utils/lst_pid.c
@@ -6,22 +6,24 @@
 /*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 11:02:26 by vitosant          #+#    #+#             */
-/*   Updated: 2025/10/07 11:54:18 by vitosant         ###   ########.fr       */
+/*   Updated: 2025/10/11 12:07:08 by vitosant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void pid_add(t_minishell *shell, pid_t pid)
+void pid_add(t_minishell *shell, pid_t pid, char is_builtin, int rbuiltin)
 {
 	t_lstpid	*node;
 	t_lstpid	*lst;
 
-	node = gc_calloc(sizeof(t_lstpid), shell->gc, GC_CUSTOM2);
+	node = gc_calloc(sizeof(t_lstpid), shell->gc, GC_PIDLIST);
 	if (!node)
 		exit_code(shell, errno);
 	node->pid = pid;
 	lst = shell->lst_pid;
+	node->is_builtin = is_builtin;
+	node->rbuiltin = rbuiltin;
 	if (!lst)
 		shell->lst_pid = node;
 	else
@@ -39,8 +41,11 @@ int	get_return(t_minishell *shell)
 	grepper = 0;
 	lst = shell->lst_pid;
 	shell->lst_pid = lst->next;
-	waitpid(lst->pid, &grepper, 0);
+	if (lst->is_builtin)
+		grepper = lst->rbuiltin;
+	else
+		waitpid(lst->pid, &grepper, 0);
 	grepper = (grepper & 0xFF00) >> 8;
-	gc_delptr(lst, shell->gc, GC_CUSTOM2);
+	gc_delptr(lst, shell->gc, GC_PIDLIST);
 	return (grepper);
 }
