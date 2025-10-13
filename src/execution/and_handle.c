07705757d921f3fbd1b094fcaf1d26486674ce65
vitosant <vitor.200483@gmail.com>
2025-10-13 15:45:38 -0300

    change line of shell->exit

--- src/execution/and_handle.c
+++ src/execution/and_handle.c
@@ -6,7 +6,7 @@
 /*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 10:06:15 by vitosant          #+#    #+#             */
-/*   Updated: 2025/10/11 12:13:00 by vitosant         ###   ########.fr       */
+/*   Updated: 2025/10/13 15:43:39 by vitosant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	and_node(t_minishell *shell, t_ast *node)
 
 	executor(shell, node->left);
 	grepper = get_return(shell);
+	shell->exit = grepper;
 	if (grepper == 0)
 		executor(shell, node->right);
-	shell->exit = grepper;
 }
