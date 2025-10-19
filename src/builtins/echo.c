f12ec848a7119c222581a69fa57bafaa38ee2892
vitosant <vitor.200483@gmail.com>
2025-10-19 18:26:40 -0300

    Finish scope of echo builtin

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -11,3 +11,44 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	echo_builtin(t_minishell *shell, t_cmd *cmd)
+{
+	int	i;
+	int	j;
+	int	put_endln;
+
+	i = 1;
+	put_endln = 1;
+	while (cmd->argv[i] && cmd->argv[i][0] == '-')
+	{
+		j = 1;
+		while (cmd->argv[i][j])
+		{
+			if (ft_strchr("n", cmd->argv[i][j]) == NULL)
+			{
+				just_echo(&cmd->argv[i], put_endln);
+				return ;
+			}
+			j++;
+		}
+		put_endln = 0;
+		i++;
+	}
+	just_echo(&cmd->argv[i], put_endln);
+	pid_add(shell, NOT_FORKED, NOT_FORKED, 0);
+}
+
+static void	just_echo(char **argv, int put_endln)
+{
+	int	i;
+
+	while (argv[i])
+	{
+		ft_putstr_fd(argv[i], 1);
+		ft_putchar_fd(' ', 1);
+		i++;
+	}
+	if (put_endln)
+		ft_putchar_fd('\n', 1);
+}
