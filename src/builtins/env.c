110da06593511dc46b7eaa80a01b521872d21dee
vitosant <vitor.200483@gmail.com>
2025-10-19 17:04:52 -0300

    finish scope of env builtin

---
+++ src/builtins/env.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vitosant <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 16:55:06 by vitosant          #+#    #+#             */
+/*   Updated: 2025/10/19 16:55:08 by vitosant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	env_builtin(t_minishell *shell, t_cmd *cmd)
+{
+	char	**env;
+
+	env = shell->env;
+	if (cmd->argv[1])
+	{
+		ft_putstr_fd("env: Too many arguments", 2);
+		pid_add(shell, NOT_FORKED, NOT_FORKED, 1  << 8);
+		return ;
+	}
+	while (*env)
+	{
+		ft_putendl_fd(*env, 1);
+		env++;
+	}
+	pid_add(shell, NOT_FORKED, NOT_FORKED, 0);
+}
+
