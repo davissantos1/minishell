95ba7a8c6e4f65bdd9c14d42a5eef9d1544d2eb5
vitosant <vitor.200483@gmail.com>
2025-10-20 17:02:25 -0300

    Added exit code 126 and 127

--- src/utils/check_command.c
+++ src/utils/check_command.c
@@ -6,13 +6,13 @@
 /*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 12:01:22 by vitosant          #+#    #+#             */
-/*   Updated: 2025/10/07 12:04:49 by vitosant         ###   ########.fr       */
+/*   Updated: 2025/10/20 15:21:29 by vitosant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	check_command(t_cmd *cmd)
+int	check_command(t_minishell *shell, t_cmd *cmd)
 {
 	char	*file;
 
@@ -21,11 +21,13 @@ int	check_command(t_cmd *cmd)
 	{
 		ft_putstr_fd(file, 2);
 		ft_putstr_fd(": command not found\n", 2);
+		shell->exit = 127;
 		return (0);
 	}
 	if (access(file, X_OK) == -1)
 	{
 		perror(file);
+		shell->exit = 126;
 		return (0);
 	}
 	return (1);
