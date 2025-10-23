70c12d3e4a51c6dc9555f4dfe1d9ffb6a744fa8e
davi santos <davisimoesdossantos1@gmail.com>
2025-10-23 17:34:07 -0300

    Started working on wildcard mixed expansion

--- src/utils/expander.c
+++ src/utils/expander.c
@@ -6,7 +6,7 @@
 /*   By: dasimoes <dasimoes@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 19:35:09 by dasimoes          #+#    #+#             */
-/*   Updated: 2025/10/23 12:41:40 by dasimoes         ###   ########.fr       */
+/*   Updated: 2025/10/23 17:16:17 by dasimoes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*expand_mixed_str(t_minishell *s, char **spl, int i, char c)
 
 	pos = ft_strchr(spl[i], c);
 	strs = ft_break(spl[i], (int)(pos - spl[i]));
+	if ((!gc_addmtx(strs, s->gc, GC_TOKEN))
+		exit_code(s, EXIT_FAILURE);
 	if (!strs)
 		exit_code(s, EXIT_FAILURE);
 	if (spl[i][0] == c)
@@ -31,13 +33,14 @@ char	*expand_mixed_str(t_minishell *s, char **spl, int i, char c)
 void	expand_mixed_wildcard(t_minishelll *s, char ***spl, int i)
 {
 	char	*dol;
+	char	*var;
 	char	*wild;
 	char	**strs;
 
 	dol = ft_strchr((*spl)[i], '$');
 	wild = ft_strchr((*spl)[i], '*');
 	strs = ft_break((*spl)[i], (int)(wild - (*spl)[i]);
-	if (get_env(s, dol
+	var = get_env(s, dol
 	if (!strs)
 		exit_code(s, EXIT_FAILURE);
 	if (spl[i][0] == '*')
@@ -45,6 +48,4 @@ void	expand_mixed_wildcard(t_minishelll *s, char ***spl, int i)
 	else
 		strs[0] = get_env(s, strs[0]);
 	return (ft_strjoin(strs[0], strs[1]));
-	
-
 }
