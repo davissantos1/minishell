090977a437efb0e34361cc639026522b7f9d55d0
davi santos <davisimoesdossantos1@gmail.com>
2025-11-10 20:24:45 -0300

    Fixed tokenizer bug on glued quotes

--- src/parsing/tokenizer.c
+++ src/parsing/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 15:45:28 by dasimoes          #+#    #+#             */
-/*   Updated: 2025/11/10 16:37:03 by dasimoes         ###   ########.fr       */
+/*   Updated: 2025/11/10 18:33:30 by dasimoes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ char	*token_quotes(t_minishell *s, int i)
 			closed = 0;
 		else if (s->input[j] && s->input[j] == s->input[i] && !closed)
 			closed = 1;
-		else if (is_space(s->input[j]) && closed)
+		else if ((is_space(s->input[j]) || is_operator(s->input[j])) && closed)
 			break ;
 		else if (s->input[j] == '\0')
 			break ;
