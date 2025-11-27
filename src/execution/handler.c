f6f6727f576e08e3146b305cd066b64dc0cfcfd3
vitosant <vitor.200483@gmail.com>
2025-11-27 14:40:19 -0300

    Fix sigint bug in heredoc

--- src/execution/handler.c
+++ src/execution/handler.c
@@ -6,7 +6,7 @@
 /*   By: vitosant <vitosant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 08:14:39 by vitosant          #+#    #+#             */
-/*   Updated: 2025/11/10 21:11:32 by dasimoes         ###   ########.fr       */
+/*   Updated: 2025/11/27 14:39:30 by vitosant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ void	handler(t_minishell *shell)
 		shell->exit = g_signal + 128;
 	g_signal = 0;
 	signal(SIGINT, sigint_heredoc);
-	register_parent_signals();
 	find_heredoc(shell, shell->root);
+	register_parent_signals();
 	build_path(shell);
 	if (g_signal != 2)
 		executor(shell, shell->root);
